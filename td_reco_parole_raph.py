336fb1237cacd290b572650c62acf251a09f6645
raphaelchalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-12-02 10:15:56 +0100

    Calcul lpc

--- td_reco_parole_raph.py
+++ td_reco_parole_raph.py
@@ -5,22 +5,74 @@ Created on Wed Dec  2 08:50:42 2020
 
 @author: raphael
 """
-
+#%% Imports
 import scipy.io.wavfile as wav
+from scipy.linalg import toeplitz
 import numpy as np
+from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
+#%% Paramètres globaux
 chemin = "spoken_digit_dataset/"
-loc  = ["jackson", "jason", "nicolas", "theo"]
+locuteurs  = ["jackson", "jason", "nicolas", "theo"]
 
-def genere_nom(c, l, o):
-    return chemin + str(c) + '_' + loc[l] + '_' + str(o) + '.wav'
+#%% Fonctions
+def genere_nom(nbr_prononce, index_locuteur, essai):
+    return chemin + str(nbr_prononce) + '_' + locuteurs[index_locuteur] + '_' + str(essai) + '.wav'
+
+def calcul_lpc_fenetre(fenetre, ordre):
+    """
+    Parameters
+    ----------
+    fenetre : Array
+        DESCRIPTION.
+    ordre : Int
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        Coefficients lpc pour une fenêtre donnée.
+
+    """
+    t_fenetre = len(fenetre)
+    R = np.zeros((ordre + 1, 1))
+    for k in range(ordre + 1):
+        R[k] = np.mean(fenetre[0:t_fenetre - k - 1] * fenetre[k:t_fenetre - 1])
+    m_R = toeplitz(R)
+    
+    v = np.zeros((ordre + 1, 1))
+    v[0] = 1
+    
+    lpc = np.dot(inv(m_R), v)
+    lpc = lpc/lpc[0]
+    return lpc[1:]
+
+def calcul_lpc(data, samplerate, ordre, taille_fenetre=0.02):
+    taille_data_fenetre = int(samplerate*taille_fenetre)
+    taille_decalage = taille_data_fenetre//2
+    taille_data = data.shape[0]
+    taille_mat_lpc = taille_data//taille_decalage
+    matrice_lpc = np.zeros((ordre, taille_decalage))
+    for k in range(taille_mat_lpc):
+        fenetre_inf = k*taille_decalage
+        fenetre_sup = fenetre_inf + taille_data_fenetre
+        matrice_lpc[:,k] = calcul_lpc_fenetre(data[fenetre_inf:fenetre_sup], ordre).T
+    return matrice_lpc
 
 if __name__ == '__main__':
+    ordre_modele = 10
     filename = genere_nom(3, 0, 0)
-    # Fs : int, Sample rate of wav file
-    # s : numpy array, Data read from wav file
-    [Fs, s] = wav.read(filename)
-    nb_sounds = len(s)
-    t = np.linspace(0, nb_sounds/Fs, nb_sounds)
-    plt.plot(t, s)
+    samplerate, data = wav.read(filename)
+    # length = data.shape[0] / samplerate
+    # time = np.linspace(0., length, data.shape[0])
+    # plt.plot(time, data, label=filename)
+    # plt.legend()
+    # plt.xlabel("Time [s]")
+    # plt.ylabel("Amplitude")
+    # plt.show()
+    
+    # lpc = calcul_lpc_fenetre(data[0:1600], ordre_modele)
+    matrice_lpc = calcul_lpc(data, samplerate, ordre_modele)
+    
+    
